e19c7228e0526c25e4a85443502fc0ed63f2f21d
陈小梅 <wb.chenxiaomei01@mesg.corp.netease.com>
2021-02-08 15:59:13 +0800

    feat: 1. webpack-merge 此工具会引用 "common" 配置，因此我们不必再在环境特定(environment-specific)的配置中编写重复代码。

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
-// import * as _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import { cube } from "./test";
-
 function component() {
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Looks like we are in development mode!");
+  }
   var btn = document.createElement("button");
   btn.innerHTML = "Click me and check the console!";
-//   btn.onclick = testFun;
 
   const element = document.createElement("div");
-  //   element.innerHTML = "111cxm333";
-  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
-    "\n\n"
-  );
   element.classList.add("hello");
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
   element.appendChild(btn);
   return element;
 }
 const element = component();
 document.body.appendChild(element);
-
-// if (module.hot) {
-//     module.hot.accept('./test.js', function () {
-//         console.log('Accepting the updated printMe module!');
-//         testFun();
-//         document.body.removeChild(element);
-//         const element = component()
-//         document.body.appendChild(element);
-//     })
-// }
