084e35bc938d964ecb8a21e6e330e46c689d86bb
陈小梅 <wb.chenxiaomei01@mesg.corp.netease.com>
2021-02-21 14:23:09 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function component() {
   if (process.env.NODE_ENV !== "production") {
-    console.log("Looks like we are in development mode!");
+    console.log("Looks like we are in development mode!!");
   }
   var btn = document.createElement("button");
   btn.innerHTML = "Click me and check the console!";
