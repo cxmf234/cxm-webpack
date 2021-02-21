a680af39783fb474f4a0e2eb75fece685af0f888
陈小梅 <wb.chenxiaomei01@mesg.corp.netease.com>
2021-02-21 15:00:59 +0800

    缓存

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import _ from "lodash";
+// import Print from "./print";
+
 function component() {
   if (process.env.NODE_ENV !== "production") {
     console.log("Looks like we are in development mode!!");
   }
+
+  console.log(_.join(["Another", "module", "loaded!"], " "));
+
   var btn = document.createElement("button");
   btn.innerHTML = "Click me and check the console!";
+//   btn.onclick = Print.bind(null, "Hello webpack!");
 
   const element = document.createElement("div");
   element.classList.add("hello");
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -11,9 +12,27 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Output Management",
     }),
+    /*  main bundle 会随着自身的新增内容的修改，而发生变化。
+        vendor bundle 会随着自身的 module.id 的变化，而发生变化
+        manifest bundle 会因为现在包含一个新模块的引用，而发生变化。
+        HashedModuleIdsPlugin用来解决不论是否添加任何新的本地依赖，对于前后两次构建，vendor hash 都应该保持一致
+    */
+    new webpack.HashedModuleIdsPlugin(),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
