066c4430722a15fe98df36b4494766c2807d5e33
陈小梅 <wb.chenxiaomei01@mesg.corp.netease.com>
2021-03-11 13:54:18 +0800

    feat: webpack-dashboard

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
   entry: {
@@ -11,6 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Output Management",
     }),
+    new DashboardPlugin(),
   ],
   output: {
     filename: "[name].bundle.js",
